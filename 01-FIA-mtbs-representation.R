66088c2cb40135da70f8dec11962df6a0153aabf
Anjum Gujral <akgujral@ucdavis.edu>
2024-12-19 17:32:36 -0800

    looking at representation of FIA plots in big fires

---
+++ 01-FIA-mtbs-representation.R
@@ -0,0 +1,23 @@
+
+library(sf)
+library(terra)
+library(dplyr)
+library(raster)
+
+#load points for fuzzed FIA plots
+DATADIR = "/Users/anjumgujral/Box Sync/yuba-phys_data/FIA"
+FIA <- read.csv("/Users/anjumgujral/Box Sync/yuba-phys_data/FIA/CA_PLOT.csv")
+
+
+
+#load fire perimeters
+
+
+
+#load mtbs
+
+
+#find the intersection of points between FIA plots and mtbs within
+```
+
+
